08e96cba5cc1910ab2c452c0ef920b923fb59da3
theyes@thoughtworks.com <theyes@thoughtworks.com>
2021-06-04 10:49:44 +0200

    Spanishify the bot

--- vactrack.py
+++ vactrack.py
@@ -2,10 +2,11 @@
 
 # vactrack.py code by @thetafferboy
 
-import pandas as pd
+import sys
+# import pandas as pd
 import tweepy
 from datetime import datetime
-from pandas_ods_reader import read_ods
+# from pandas_ods_reader import read_ods
 import urllib.request
 
 # # Twitter authorisation - you need to fill in your own API details (https://dev.twitter.com)
@@ -16,57 +17,81 @@ import urllib.request
 # You can change population of UK if you wish, which will change % calculations
 population_of_spain = 47332614
 
-# How many blocks you want in progress bar, 15 works well with Twitter ▓▓▓▓▓░░░░░░░░░░
-bar_total = 15
-perc_per_bar = 100/bar_total
 
-# This sets date to 2 days ago, as there is a lag in government data reporting. API requests will fail if you request date which has no data yet
-from datetime import date, timedelta
-date_to_check = date.today().isoformat()
 
-# df = read_ods('https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_20210125.ods', "Hoja3")
+import tweepy
 
 today = date.today().strftime('%Y%m%d')
 print(today)
 
+# calculate today's date and format it to match
+# the ods file url from the .gob website
+from datetime import date, timedelta
+date_to_check = date.today()
+today = date_to_check.strftime('%Y%m%d')
 baseurl = 'https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_'
 full_url = baseurl + today + '.ods'
 
+# download and read today's data
+# into a pandas data frame
+import pandas
+from pandas_ods_reader import read_ods
+
 dataFile = 'data.ods'
-
 urllib.request.urlretrieve(full_url, dataFile)
+# Comunicación is a specifc sheet in the .ods dataFile
+# This is quite brittle and if anything is going to break...
+df = read_ods(dataFile, "Comunicación")
 
-df = read_ods(dataFile, "Hoja3") # if anything is going to break...
 
-number_of_vaccines_given = df.loc[19,'Dosis administradas (2)'] # or maybe this!
-number_of_people_given_2_doses = df.loc[19,'Nº Personas vacunadas\n(pauta completada)']
+# read the data from the downloaded file also brittle
+# but I'd sooner it break that tweet something wrong
+number_of_vaccines_given = df.loc[20,'Dosis administradas (2)']
+number_of_people_given_2_doses = df.loc[20,'Nº Personas vacunadas\n(pauta completada)']
+
+print("Total vacs given: ")
+print(number_of_vaccines_given)
+print("Total given 2 doses: ")
+print(+ number_of_people_given_2_doses)
 
 number_of_people_given_1_dose = number_of_vaccines_given - number_of_people_given_2_doses
 
-def BuildLoadingBars(vacines_given):
-    perc_rounded = round( ((vacines_given / population_of_spain) * 100),2)
+def handler(event, context):
+    return 'Hello from AWS Lambda using Python' + sys.version + '!'
 
+def BuildLoadingBars(vaccines_given):
+    population_of_spain = 47332614
+    # How many blocks you want in progress bar
+    # 15 works well with Twitter ▓▓▓▓▓░░░░░░░░░░
+    bar_total = 15
+    perc_per_bar = 100 / bar_total
+
+    perc_rounded = round( ((vaccines_given / population_of_spain) * 100),2)
+
+    # floor / integer division you can't
+    # have .5 of a char based loading bar
     solid_bars_to_print = int(perc_rounded // perc_per_bar)
     empty_bars_to_print = int(bar_total - solid_bars_to_print)
 
+    # python lets you multiply chars and I think it is cute
     dataToAdd =  '▓' * solid_bars_to_print
     dataToAdd += '░' * empty_bars_to_print
     dataToAdd += ' ' + str(perc_rounded) + '%\n\n'
     return dataToAdd
 
-# def SendTweet(stringToTweet):
-    # api.update_status(stringToTweet)
+def SendTweet(tweet):
+    api.update_status(tweet)
 
-stringToTweet = 'A día '+ str(date_to_check)+ '\n\n'
-stringToTweet += 'Personas que han recibido la 1ª vacuna: \n'
-stringToTweet += BuildLoadingBars(number_of_people_given_1_dose)
-stringToTweet += 'Personas que han recibido la 2ª vacuna: \n'
-stringToTweet += BuildLoadingBars(number_of_people_given_2_doses)
-stringToTweet += 'Según datos de @sanidadgob\n'
-stringToTweet += '#VacunaCOVID19 #YoMeVacuno\n'
-stringToTweet += 'https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/vacunaCovid19.htm'
+tweet = 'A día '+ str(date_to_check.isoformat())+ '\n\n'
+tweet += 'Personas que han recibido la 1ª vacuna: \n'
+tweet += BuildLoadingBars(number_of_people_given_1_dose)
+tweet += 'Personas que han recibido la 2ª vacuna: \n'
+tweet += BuildLoadingBars(number_of_people_given_2_doses)
+tweet += 'Según datos de @sanidadgob\n'
+tweet += '#VacunaCOVID19 #YoMeVacuno\n'
+tweet += 'https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/vacunaCovid19.htm'
 
-print(stringToTweet)
-print(len(stringToTweet))
+print(tweet)
+print(len(tweet))
 
-# SourceAndSendTweet(stringToTweet)
+# SendTweet(tweet)
